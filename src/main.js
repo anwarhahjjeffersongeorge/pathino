f26f1f8a9f1092fe8b6cc22157ddb7a13722fa98
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2020-03-08 05:32:14 -0700

    refactor, docs

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
 // Parse a dot notated path, remove spaces
 const dotParse = path => path.split(/\./).filter(substring => substring)
 
-// Deep find element in an object by path elements p, returning undefined if the path was nonexistent or undefined
-const getPathInO = (o = {}, ...p) =>
-  p.reduce(({no, quit}, sp) => {
+// Deep find element in an object by path elements p, returning undefined if the path or element at the path was undefined
+const getPathInO = (original = {}, ...paths) =>
+  paths.reduce(({no, quit}, sp) => {
       return sp in no && !quit
         ? {no: no[sp], quit: false}
         : {no: undefined, quit: true}
     },
-    {no: o, quit: false}).no
+    {no: original, quit: false}).no
 
 // Deep find something in an object using dot-notation path
-const dotGetPathInO = (o, p) => getPathInO(o, ...dotParse(p))
+const dotGetPathInO = (original, pathstring) => getPathInO(original, ...dotParse(pathstring))
 
 // Deep put something in an object by path elements p, creating tree as needed, using Object.assign
-const putPathInO = (o = {}, v, ...p) =>
-  p.reduce(
+const putPathInO = (original = {}, value, ...paths) =>
+  paths.reduce(
     ({o, no}, sp, i, {length}, j = length - 1) => {
       return i < j
         ? sp in no
           ? {o, no: no[sp]}
           : {o, no: Object.assign(no, {[sp]: {}})[sp]}
-        : {o, no: Object.assign(no, {[sp]:v})}
+        : {o, no: Object.assign(no, {[sp]: value})}
     },
-    {o, no: o}).o
+    {o: original, no: original}).o
 
 // Deep put something in an object using dot-notation path and Object.assign
-const dotPutPathInO = (o, v, p) => putPathInO(o, v, ...dotParse(p))
+const dotPutPathInO = (original, value, pathstring) => putPathInO(original, value, ...dotParse(pathstring))
 
 export {dotParse, getPathInO, dotGetPathInO, putPathInO, dotPutPathInO}
